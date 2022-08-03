a30ececf0cc7acc8c13d937bb5f159f2c8ca333a
Christian Skorobogatow <cskorobogatow@deloitte.es>
2022-08-03 08:52:56 +0200

    Initial Commit

---
+++ MatchGeometryEffect/MatchGeometryEffectApp.swift
@@ -0,0 +1,17 @@
+//
+//  MatchGeometryEffectApp.swift
+//  MatchGeometryEffect
+//
+//  Created by Skorobogatow, Christian on 3/8/22.
+//
+
+import SwiftUI
+
+@main
+struct MatchGeometryEffectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
